740a904ea2e013ed6d98d276b8c9244363768cb8
Alexandr Sarandi <sarandi.alex@gmail.com>
2019-11-25 16:39:32 -0800

    moves table

--- Makefile
+++ Makefile
@@ -3,11 +3,11 @@ CFLAGS  += -g -Wall #-Werror
 CFLAGS  += $(shell sdl2-config --cflags)
 LDFLAGS += $(shell sdl2-config --libs) -lGL -lGLU -lm
 
-SRC = cube.c faces.c main.c
+SRC = cube.c faces.c main.c queue.c
 OBJ = $(SRC:.c=.o)
 
 $(NAME): $(OBJ)
-	$(CC) $(CFLAGS) $^ $(LDFLAGS) -o $@
+	$(CC) -o $@ $^ $(LDFLAGS)
 
 clean:
 	rm -f $(OBJ)
--- main.c
+++ main.c
@@ -15,6 +15,30 @@ void update_left();
 
 extern float c[6][3];
 
+enum moves
+{
+    F,
+    F_PRIME,
+    F_2,
+    B,
+    B_PRIME,
+    B_2,
+    U,
+    U_PRIME,
+    U_2,
+    D,
+    D_PRIME,
+    D_2,
+    R,
+    R_PRIME,
+    R_2,
+    L,
+    L_PRIME,
+    L_2,
+
+} move;
+
+
 SDL_GLContext   *context;
 SDL_Window      *window;
 
@@ -119,7 +143,6 @@ t_cube *make_cube(float x, float y, float z)
     return &t;
 }
 
-float angle = -0.0174533; //-1.5708f;
 
 /*
 
@@ -133,21 +156,34 @@ anti-clockwise
 3 4 5  =>  1 4 7
 6 7 8      0 3 6
 
+twice
+0 1 2      8 7 6
+3 4 5  =>  5 4 3
+6 7 8      2 1 0
+
 */
 
-void rotate_ints(int *face, int rev)
+
+
+void rotate_ints(int *face, int k)
 {
-    int clockwise[] = {6, 3, 0, 7, 4, 1, 8, 5, 2};
-    int anticlock[] = {2, 5, 8, 1, 4, 7, 0, 3, 6};
-    int *src, copy[9], i;
+    int rot[3][9] = {
+        {6, 3, 0, 7, 4, 1, 8, 5, 2},
+        {2, 5, 8, 1, 4, 7, 0, 3, 6},
+        {8, 7, 6, 5, 4, 3, 2, 1, 0}
+        };
+
+    int copy[9], i;
 
     for (i=0; i<9; i++)
         copy[i] = face[i];
-    src = rev ? anticlock : clockwise;
     for (i=0; i<9; i++)
-        face[i] = copy[src[i]];
+        face[i] = copy[rot[k][i]];
 }
 
+
+//float angle = -0.0174533; // one degree
+float angle = -1.5708f;
 
 void rotate_Z(int *face, int rev)   //F, B
 {
@@ -157,7 +193,7 @@ void rotate_Z(int *face, int rev)   //F, B
         for (int j=0; j<6; j++)
         {
             for (int k=0; k<4; k++)
-                rotateZ(cubes[i][j][k], rev ? -angle : angle);
+                rotateZ(cubes[i][j][k], angle * rev);
         }
     }
 }
@@ -170,7 +206,7 @@ void rotate_Y(int *face, int rev)   //U, D
         for (int j=0; j<6; j++)
         {
             for (int k=0; k<4; k++)
-                rotateY(cubes[i][j][k], rev ? -angle : angle);
+                rotateY(cubes[i][j][k], angle * rev);
         }
     }
 }
@@ -184,7 +220,7 @@ void rotate_X(int *face, int rev)   //R, L
         for (int j=0; j<6; j++)
         {
             for (int k=0; k<4; k++)
-                rotateX(cubes[i][j][k], rev ? -angle : angle);
+                rotateX(cubes[i][j][k], angle * rev);
         }
     }
 }
@@ -260,31 +296,86 @@ uint64_t get_time_stamp()
 }
 
 #define MOVE_DURATION 500000    /* half second */
-static uint64_t move_started_time;
+//static uint64_t move_started_time;
 
+typedef struct s_move
+{
+    enum moves m;
+    int     sdlk;
+    int     kmod;
+    char    *str;
+    int     face;
+    void    (*f)();
+    int     dir;
+    int     rot;
+    void    (*u)();
+} t_move;
+
+t_move  all_moves[] = {
+
+//
+//    //move   sdlk,   kmod,        str,  face func     rot int  update
+    { F,       SDLK_f, KMOD_NONE  , "F" , 0, &rotate_Z,  1, 0, &update_front },
+    { F_PRIME, SDLK_f, KMOD_LSHIFT, "F'", 0, &rotate_Z, -1, 1, &update_front },
+    { F_2,     SDLK_f, KMOD_LCTRL , "F2", 0, &rotate_Z,  2, 2, &update_front },
+    { B,       SDLK_b, KMOD_NONE  , "B" , 1, &rotate_Z, -1, 0, &update_back  },
+    { B_PRIME, SDLK_b, KMOD_LSHIFT, "B'", 1, &rotate_Z,  1, 1, &update_back  },
+    { B_2,     SDLK_b, KMOD_LCTRL , "B2", 1, &rotate_Z,  2, 2, &update_back  },
+    { U,       SDLK_u, KMOD_NONE  , "U" , 2, &rotate_Y,  1, 0, &update_up    },
+    { U_PRIME, SDLK_u, KMOD_LSHIFT, "U'", 2, &rotate_Y, -1, 1, &update_up    },
+    { U_2,     SDLK_u, KMOD_LCTRL , "U2", 2, &rotate_Y,  2, 2, &update_up    },
+    { D,       SDLK_d, KMOD_NONE  , "D" , 3, &rotate_Y, -1, 0, &update_down  },
+    { D_PRIME, SDLK_d, KMOD_LSHIFT, "D'", 3, &rotate_Y,  1, 1, &update_down  },
+    { D_2,     SDLK_d, KMOD_LCTRL , "D2", 3, &rotate_Y,  2, 2, &update_down  },
+    { R,       SDLK_r, KMOD_NONE  , "R" , 4, &rotate_X,  1, 0, &update_right },
+    { R_PRIME, SDLK_r, KMOD_LSHIFT, "R'", 4, &rotate_X, -1, 1, &update_right },
+    { R_2,     SDLK_r, KMOD_LCTRL , "R2", 4, &rotate_X,  2, 2, &update_right },
+    { L,       SDLK_l, KMOD_NONE  , "L" , 5, &rotate_X, -1, 0, &update_left  },
+    { L_PRIME, SDLK_l, KMOD_LSHIFT, "L'", 5, &rotate_X,  1, 1, &update_left  },
+    { L_2,     SDLK_l, KMOD_LCTRL , "L2", 5, &rotate_X,  2, 2, &update_left  }
+};
 
 void handleKeyPress(SDL_Keysym *keysym) {
+
+    for (int i=0; i<18; i++)
+    {
+        if (all_moves[i].sdlk != keysym->sym)
+            continue ;
+        if ((!all_moves[i].kmod) && (keysym->mod))
+            continue ;
+        if ((all_moves[i].kmod) && (keysym->mod != all_moves[i].kmod))
+            continue ;
+
+        int f = all_moves[i].face;
+        all_moves[i].f(faces[f], all_moves[i].dir);
+        rotate_ints(faces[f], all_moves[i].rot);
+        all_moves[i].u();
+        printf("%s\n", all_moves[i].str);
+        break ;
+
+    }
+
     switch (keysym->sym) {
         case SDLK_ESCAPE:
             Quit(0);
             break;
-        case SDLK_a: {rotate_Z(faces[0], 0); rotate_ints(faces[0], 0); update_front(); printf("F \n"); break ;} //F
-        case SDLK_z: {rotate_Z(faces[0], 1); rotate_ints(faces[0], 1); update_front(); printf("F'\n"); break ;} //F'
-
-        case SDLK_s: {rotate_Z(faces[1], 1); rotate_ints(faces[1], 0); update_back (); printf("B \n"); break ;} //B
-        case SDLK_x: {rotate_Z(faces[1], 0); rotate_ints(faces[1], 1); update_back (); printf("B'\n"); break ;} //B'
-
-        case SDLK_d: {rotate_Y(faces[2], 0); rotate_ints(faces[2], 0); update_up   (); printf("U \n"); break ;} //U
-        case SDLK_c: {rotate_Y(faces[2], 1); rotate_ints(faces[2], 1); update_up   (); printf("U'\n"); break ;} //U'
-
-        case SDLK_f: {rotate_Y(faces[3], 1); rotate_ints(faces[3], 0); update_down (); printf("D \n"); break ;} //D
-        case SDLK_v: {rotate_Y(faces[3], 0); rotate_ints(faces[3], 1); update_down (); printf("D'\n"); break ;} //D'
-
-        case SDLK_g: {rotate_X(faces[4], 0); rotate_ints(faces[4], 0); update_right(); printf("R \n"); break ;} //R
-        case SDLK_b: {rotate_X(faces[4], 1); rotate_ints(faces[4], 1); update_right(); printf("R'\n"); break ;} //R'
-
-        case SDLK_h: {rotate_X(faces[5], 1); rotate_ints(faces[5], 0); update_left (); printf("L \n"); break ;} //L
-        case SDLK_n: {rotate_X(faces[5], 0); rotate_ints(faces[5], 1); update_left (); printf("L'\n"); break ;} //L'
+//        case SDLK_a: {rotate_Z(faces[0], 0); rotate_ints(faces[0], 0); update_front(); printf("F \n"); break ;} //F
+//        case SDLK_z: {rotate_Z(faces[0], 1); rotate_ints(faces[0], 1); update_front(); printf("F'\n"); break ;} //F'
+//
+//        case SDLK_s: {rotate_Z(faces[1], 1); rotate_ints(faces[1], 0); update_back (); printf("B \n"); break ;} //B
+//        case SDLK_x: {rotate_Z(faces[1], 0); rotate_ints(faces[1], 1); update_back (); printf("B'\n"); break ;} //B'
+//
+//        case SDLK_d: {rotate_Y(faces[2], 0); rotate_ints(faces[2], 0); update_up   (); printf("U \n"); break ;} //U
+//        case SDLK_c: {rotate_Y(faces[2], 1); rotate_ints(faces[2], 1); update_up   (); printf("U'\n"); break ;} //U'
+//
+//        case SDLK_f: {rotate_Y(faces[3], 1); rotate_ints(faces[3], 0); update_down (); printf("D \n"); break ;} //D
+//        case SDLK_v: {rotate_Y(faces[3], 0); rotate_ints(faces[3], 1); update_down (); printf("D'\n"); break ;} //D'
+//
+//        case SDLK_g: {rotate_X(faces[4], 0); rotate_ints(faces[4], 0); update_right(); printf("R \n"); break ;} //R
+//        case SDLK_b: {rotate_X(faces[4], 1); rotate_ints(faces[4], 1); update_right(); printf("R'\n"); break ;} //R'
+//
+//        case SDLK_h: {rotate_X(faces[5], 1); rotate_ints(faces[5], 0); update_left (); printf("L \n"); break ;} //L
+//        case SDLK_n: {rotate_X(faces[5], 0); rotate_ints(faces[5], 1); update_left (); printf("L'\n"); break ;} //L'
 
         case SDLK_RIGHT: perspective_y += 11.25f; break ;
         case SDLK_LEFT:  perspective_y -= 11.25f; break ;
